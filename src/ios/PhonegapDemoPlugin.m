90740db2100706a1b25e3185367363ab6e657dc7
raneswapnils <raneswapnils@gmail.com>
2023-04-13 15:28:39 +0530

    Update PhonegapDemoPlugin.m
    Updated method name

--- src/ios/PhonegapDemoPlugin.m
+++ src/ios/PhonegapDemoPlugin.m
@@ -6,12 +6,12 @@
   // Member variables go here.
 }
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)testMethod:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation PhonegapDemoPlugin
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
+- (void)testMethod:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
